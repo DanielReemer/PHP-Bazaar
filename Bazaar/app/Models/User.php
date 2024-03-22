95a5e16cdea248d55ccb851c589a18ac1f48ad9e
DanielReemer <82873259+DanielReemer@users.noreply.github.com>
2024-03-22 17:13:48 +0100

    fix: fixed issue caused by rebase

--- Bazaar/app/Models/User.php
+++ Bazaar/app/Models/User.php
@@ -4,10 +4,10 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class User extends Authenticatable
 {
@@ -44,7 +44,7 @@ class User extends Authenticatable
         'password' => 'hashed',
     ];
 
-    public function role(): hasOne
+    public function role(): BelongsTo
     {
         return $this->belongsTo(Role::class);
     }
