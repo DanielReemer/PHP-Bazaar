b3519dadc1df5e6c078ae56bc2c2b3e67cde8e36
DanielReemer <82873259+DanielReemer@users.noreply.github.com>
2024-03-25 21:25:51 +0100

    Localization for csv uploading

--- Bazaar/app/Http/Controllers/AdvertController.php
+++ Bazaar/app/Http/Controllers/AdvertController.php
@@ -9,6 +9,7 @@ use App\Models\Advert;
 use App\Models\AdvertQueue;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Auth;
 
@@ -30,7 +31,17 @@ class AdvertController extends Controller
      */
     public function create() : View
     {
-        return view('adverts.new-advert');
+        $data = [
+            'createLabel' => trans()->get('advertPostForm.create'),
+            'titleLabel' => trans()->get('advertPostForm.title'),
+            'descriptionLabel' => trans()->get('advertPostForm.description'),
+            'isRentalLabel' => trans()->get('advertPostForm.is_rental'),
+            'postButtonText' => trans()->get('advertPostForm.post'),
+            'uploadFileLabel' => trans()->get('advertPostForm.csvFile'),
+            'uploadButtonText' => trans()->get('advertPostForm.upload')
+        ];
+
+        return view('adverts.new-advert', $data);
     }
 
     /**
@@ -56,7 +67,7 @@ class AdvertController extends Controller
         if (! ($this->limitCheck($isRental))) {
             return redirect()->back()->with('error', 'Maximum number of ads have been posted.');
         }
-        $advert = $this->createNewAdvert($request->title, $request->description, (int)$isRental);
+        $advert = $this->createNewAdvert($request->title, $request->description, (int) $isRental);
         $advert->save();
 
         // TODO: Change to a sort of dashboard; 
@@ -77,9 +88,7 @@ class AdvertController extends Controller
             $this->saveAdvertsFromQueue();
         } catch (Exception $e) {
             return redirect()->back()->with('error', $e->getMessage());
-        }
-        finally
-        {
+        } finally {
             $this->advertQueue->reset();
         }
 
@@ -158,7 +167,7 @@ class AdvertController extends Controller
         $description = $record['Description'] ?? throw new Exception('Could not find description');
         $is_rental = filter_var($record['Is_Rental'], FILTER_VALIDATE_BOOL) ?? throw new Exception('Could not determine whether the post is a rental');
 
-        if (($currentNumberOfNormalPosts >= AdvertController::MAX_ADVERT_NUM && !($is_rental)) || ($currentNumberOfRentals >= AdvertController::MAX_ADVERT_NUM && $is_rental)) {
+        if (($currentNumberOfNormalPosts >= AdvertController::MAX_ADVERT_NUM && ! ($is_rental)) || ($currentNumberOfRentals >= AdvertController::MAX_ADVERT_NUM && $is_rental)) {
             throw new Exception("Reached Maximum Number Of Posts");
         }
 
--- Bazaar/resources/views/adverts/new-advert.blade.php
+++ Bazaar/resources/views/adverts/new-advert.blade.php
@@ -1,15 +1,15 @@
 <x-app-layout>
     <div class="flex min-h-screen flex-col items-center bg-gray-100 pt-6 dark:bg-gray-900 sm:justify-center sm:pt-0">
         <header class="w-100 mt-6 text-center text-xl font-semibold text-gray-900 dark:text-white">
-            {{ __('advertPostForm.create') }}
+            {{ $createLabel }}
         </header>
-        <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md dark:bg-gray-800 sm:max-w-md sm:rounded-lg">
+        <div class="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md dark:bg-gray-800 sm:max-w-md sm:rounded-lg flex justify-start align-center flex-col gap-y-4">
             <form class="flex flex-col gap-5" method="POST" action="{{ route('new-advert') }}">
                 @csrf
 
                 <!-- Title -->
                 <div>
-                    <x-input-label for="title" :value="__('advertPostForm.title')" />
+                    <x-input-label for="title" :value="$titleLabel" />
                     <x-text-input class="mt-1 block w-full" id="title" name="title" type="text" :value="old('title')"
                         required autofocus autocomplete="title" />
                     <x-input-error class="mt-2" :messages="$errors->get('title')" />
@@ -17,7 +17,7 @@
 
                 <!-- Description -->
                 <div>
-                    <x-input-label for="description" :value="__('advertPostForm.description')" />
+                    <x-input-label for="description" :value="$descriptionLabel" />
                     <x-text-input class="mt-1 block w-full" id="description" name="description" type="text"
                         :value="old('description')" required autofocus autocomplete="description" />
                     <x-input-error class="mt-2" :messages="$errors->get('description')" />
@@ -25,7 +25,7 @@
 
                 <!-- Is rental advertisement -->
                 <div>
-                    <x-input-label for="rental" :value="__('advertPostForm.is_rental')" />
+                    <x-input-label for="rental" :value="$isRentalLabel" />
                     <input
                         class="form-checkbox rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 dark:focus:border-indigo-600 dark:focus:ring-indigo-600"
                         id="rental" name="rental" type="checkbox" value='1' />
@@ -36,23 +36,24 @@
                 <!-- submit -->
                 <div class="mt-4 flex items-center justify-start">
                     <x-primary-button class="" name="submitAdvertForm">
-                        {{ __('advertPostForm.post') }}
+                        {{ $postButtonText }}
                     </x-primary-button>
                 </div>
             </form>
-
+            <div class="mx-auto w-full border-t-2 border-white-300 my-4"></div>
             <form class="flex flex-col gap-5" method="POST" action="{{ route('new-advert-csv') }}"
                 enctype="multipart/form-data">
                 @csrf
 
                 <!-- Csv input -->
+                <x-input-label for="csv_file" :value="$uploadFileLabel" />
                 <input
-                    class="w-full appearance-none rounded border px-3 py-2 leading-tight text-gray-700 focus:border-blue-500 focus:outline-none"
+                    class="w-full appearance-none rounded border px-3 py-2 leading-tight text-white focus:border-blue-500 focus:outline-none"
                     id="csvFile" name="csv_file" type="file" accept=".csv">
                 <!-- submit -->
                 <div class="mt-4 flex items-center justify-start">
                     <x-primary-button class="" name="submitCsv">
-                        {{ __('advertPostForm.post') }}
+                        {{ $uploadButtonText }}
                     </x-primary-button>
                 </div>
             </form>
