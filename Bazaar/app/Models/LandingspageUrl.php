91fd4a90f0f7a345fd54079cd55ecf01c391bd8c
DanielReemer <82873259+DanielReemer@users.noreply.github.com>
2024-03-27 14:14:16 +0100

    Update landingsPageUrl model

--- Bazaar/app/Models/LandingspageUrl.php
+++ Bazaar/app/Models/LandingspageUrl.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class LandingspageUrl extends Model
 {
     use HasFactory;
+
+    protected $table = 'landingspage_urls';
+
+    protected $fillable = [
+        'url',
+        'advert_id',
+    ];
+
+    public function advert()
+    {
+        return $this->belongsTo(Advert::class);
+    }
 }
