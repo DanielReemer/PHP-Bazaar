8018f192e280c96e825975c182ff5ba86bd06e16
DanielReemer <82873259+DanielReemer@users.noreply.github.com>
2024-03-27 14:04:23 +0100

    Created new table, and references

---
+++ Bazaar/database/migrations/2024_03_27_125044_landings_page_url.php
@@ -0,0 +1,30 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    private string $tableName = "landingspage_urls";
+    
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create($this->tableName, function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('advert_id')->constrained('adverts')->index();
+            $table->string('url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists($this->tableName);
+    }
+};
