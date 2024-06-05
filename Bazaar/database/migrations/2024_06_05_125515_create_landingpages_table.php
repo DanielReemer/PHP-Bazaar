32cf5263aab2e478728f2eb4abc31786e642dab4
Sjul Grooten <sa.grooten@student.avans.nl>
2024-06-06 01:58:40 +0200

    refactor(migration): change value to lowercase

--- Bazaar/database/migrations/2024_06_05_125515_create_landingpages_table.php
+++ Bazaar/database/migrations/2024_06_05_125515_create_landingpages_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('landing_pages', function (Blueprint $table) {
             $table->id();
             $table->string('url')->nullable();
-            $table->string('primary_light')->default('#FFFFFF');
+            $table->string('primary_light')->default('#ffffff');
             $table->string('primary_dark')->default('#222937');
             $table->timestamps();
         });
