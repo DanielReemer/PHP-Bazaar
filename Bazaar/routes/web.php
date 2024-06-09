bda5bccc038c47a4ddb5ef04668d987017c5cc83
Sjul Grooten <sa.grooten@student.avans.nl>
2024-06-09 11:35:29 +0200

    refactor(routes): order routes

--- Bazaar/routes/web.php
+++ Bazaar/routes/web.php
@@ -24,48 +24,41 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::get('advert/{id}', [AdvertController::class, 'show'])->name('advert.show');
-Route::post('advert/{id}/comment', [AdvertReviewController::class, 'store'])->name('advertReview.store');
-Route::post('advert/{id}/favorite', [FavoriteAdvertController::class, 'update'])->name('favoriteAdvert.update');
+Route::prefix('advert')->group(function () {
+    Route::get('{id}', [AdvertController::class, 'show'])->name('advert.show');
+    Route::post('{id}/comment', [AdvertReviewController::class, 'store'])->name('advertReview.store');
+    Route::post('{id}/favorite', [FavoriteAdvertController::class, 'update'])->name('favoriteAdvert.update');
+});
 
-Route::get('favorites', [FavoriteAdvertController::class, 'show'])->name('favorites.show');
-
-Route::get('products/{type?}', [ProductController::class, 'show'])
-    ->middleware(['auth', 'verified'])
-    ->name('products.show');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('new-advert', [AdvertController::class, 'create'])
-    ->middleware('auth', 'verified', 'postingRights')
-    ->name('new-advert');
-
-Route::post('new-advert', [AdvertController::class, 'store']);
-Route::post('new-advert-csv' , [AdvertController::class,'storeCsv'])->name('new-advert-csv');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('products/{type?}', [ProductController::class, 'show'])->name('products.show');
+    Route::get('favorites', [FavoriteAdvertController::class, 'show'])->name('favorites.show');
+    Route::get('new-advert', [AdvertController::class, 'create'])->middleware('postingRights')->name('new-advert');
+    Route::post('new-advert', [AdvertController::class, 'store']);
+    Route::post('new-advert-csv', [AdvertController::class, 'storeCsv'])->name('new-advert-csv');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('profile', [PageController::class, 'updateURL'])->name('url.update');
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/admin/contract', [ContractController::class, 'show'])->name('admin.contract.show');
-    Route::POST('/admin/contract', [ContractController::class, 'store'])->name('admin.contract.store');
-    Route::get('/admin/contract/{rawFile}', [ContractController::class, 'downloadFile'])->name('admin.contract.download');
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function () {
+    Route::get('contract', [ContractController::class, 'show'])->name('admin.contract.show');
+    Route::post('contract', [ContractController::class, 'store'])->name('admin.contract.store');
+    Route::get('contract/{rawFile}', [ContractController::class, 'downloadFile'])->name('admin.contract.download');
+});
+
+Route::middleware(['auth', 'auth.editor'])->group(function () {
+    Route::get('page/{slug}/edit', [PageController::class, 'showEdit'])->name('page.showEdit');
+    Route::post('page/{slug}/edit', [PageController::class, 'update'])->name('page.update');
 });
 
 Route::get('/profile/{id}', [ProfileController::class, 'show'])->name('profile.show');
 Route::post('/profile/{id}', [UserReviewController::class, 'store'])->name('userReview.store');
-
-Route::middleware('auth', 'auth.editor')->group(function () {
-    Route::get('/page/{slug}/edit', [PageController::class, 'showEdit'])->name('page.showEdit');
-    Route::post('/page/{slug}/edit', [PageController::class, 'update'])->name('page.update');
-});
-Route::get('/page/{slug}', [PageController::class, 'show'])->name('page');
-Route::post('/profile', [PageController::class, 'updateURL'])->name('url.update');
-
+Route::get('page/{slug}', [PageController::class, 'show'])->name('page');
 
 require __DIR__.'/auth.php';
